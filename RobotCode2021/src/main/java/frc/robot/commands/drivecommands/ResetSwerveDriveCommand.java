ca352e96ab870aed688c109383abb0950220a129
Kyle Jambretz <kylejambretz@gmail.com>
2021-04-05 19:49:42 -0500

    PigeonIMU reboot 7:46PM Added code to reboot the PigeonIMU inside of ResetSwerveDriveCommand

--- RobotCode2021/src/main/java/frc/robot/commands/drivecommands/ResetSwerveDriveCommand.java
+++ RobotCode2021/src/main/java/frc/robot/commands/drivecommands/ResetSwerveDriveCommand.java
@@ -27,6 +27,7 @@ public class ResetSwerveDriveCommand extends CommandBase {
     @Override
     public void execute() {  
         driveSubsystem.swerveResetWheels();
+        driveSubsystem.rebootGyro();
         //driveSubsystem.resetSwerveOdemetry();
     }
     // Called once the command ends or is interrupted.
